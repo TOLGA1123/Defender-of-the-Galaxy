7a52072ca543c11512f3b4ccb1c852fa12cb1eaf
Mert Fidan <mert.fidan715@gmail.com>
2022-05-04 01:33:14 +0300

    tile button fixes

--- Project/src/handlers/TileHandler.java
+++ Project/src/handlers/TileHandler.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import extras.SaveAndLoad;
 
 public class TileHandler {
-    public Tile TILE_1, TILE_2, TILE_3;
+    public Tile TILE_1, TILE_2, TILE_3, TILE_4, TILE_5, TILE_6, TILE_7, TILE_8, TILE_9, TILE_10, TILE_11;
     private BufferedImage allSprites = SaveAndLoad.getAllSprites();
     public ArrayList<Tile> allTiles = new ArrayList<>();
     
@@ -20,6 +20,14 @@ public class TileHandler {
         allTiles.add(TILE_1 = new Tile(allSprites.getSubimage(160, 0, 32, 32), "roadTile", 0));
         allTiles.add(TILE_2 = new Tile(allSprites.getSubimage(32, 32, 32, 32), "starTile", 1));
         allTiles.add(TILE_3 = new Tile(allSprites.getSubimage(192, 0, 32, 32), "emptySpace", 2));
+        allTiles.add(TILE_4 = new Tile(allSprites.getSubimage(64, 0, 32, 32), "road1", 3));
+        allTiles.add(TILE_5 = new Tile(allSprites.getSubimage(96, 0, 32, 32), "road2", 4));
+        allTiles.add(TILE_6 = new Tile(allSprites.getSubimage(32, 0, 32, 32), "road3", 5));
+        allTiles.add(TILE_7 = new Tile(allSprites.getSubimage(0, 0, 32, 32), "road4", 6));
+        allTiles.add(TILE_8 = new Tile(allSprites.getSubimage(7*32, 0, 32, 32), "space2", 7));
+        allTiles.add(TILE_9 = new Tile(allSprites.getSubimage(8*32, 0, 32, 32), "space3", 8));
+        allTiles.add(TILE_10 = new Tile(allSprites.getSubimage(9*32, 0, 32, 32), "space3", 9));
+        allTiles.add(TILE_11= new Tile(allSprites.getSubimage(0, 32, 32, 32), "space4", 10));
     }
     public ArrayList<Tile> getAllTiles() {
         return allTiles;
