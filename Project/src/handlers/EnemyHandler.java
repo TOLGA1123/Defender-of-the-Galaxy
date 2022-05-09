1d947017b817847200664a76ee0e9a3c996b99c9
Mert Fidan <mert.fidan@ug.bilkent.edu.tr>
2022-05-09 03:04:51 +0300

    println deleted

--- Project/src/handlers/EnemyHandler.java
+++ Project/src/handlers/EnemyHandler.java
@@ -49,7 +49,7 @@ public class EnemyHandler {
         }
         else if(isEnd(enemy)){
             enemy.kill();
-            System.out.println("-1 lives");
+            //System.out.println("-1 lives");
         }
         else{
             newDirection(enemy);
